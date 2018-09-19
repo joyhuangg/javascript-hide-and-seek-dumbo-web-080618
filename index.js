8847e2de56d23aaaf774af01280b2cfcc66e648e
joyhuangg <joyhuangg@gmail.com>
2018-09-19 08:12:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.getElementById("nested").querySelector("div.target");
+}
+
+function increaseRankBy(n){
+  const lis = document.getElementById("app").querySelectorAll("ul.ranked-list li");
+  for (let i = 0; i < lis.length; i++){
+    lis[i].innerHTML = n + parseInt(lis[i].innerHTML);
+  }
+}
+
+function deepestChild(){
+  return document.querySelector('div#grand-node div div div div')
+}
